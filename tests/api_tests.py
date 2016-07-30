a971c1dc4a1a9c4f48f3ce6a1e48fc38940bb46e
Bernardo Roschke <bernardoroschke@gmail.com>
2016-07-30 15:41:43 +0000

    test title and body

--- tests/api_tests.py
+++ tests/api_tests.py
@@ -122,5 +122,61 @@ class TestAPI(unittest.TestCase):
         self.assertEqual(response.mimetype, "application/json")
         self.assertEqual(data["message"], "Post 1 deleted")
 
+    def test_get_posts_with_title(self):
+        """ Filtering posts by title """
+        postA = models.Post(title="Post with bells", body="Just a test")
+        postB = models.Post(title="Post with whistles", body="Still a test")
+        postC = models.Post(title="Post with bells and whistles",
+                            body="Another test")
+
+        session.add_all([postA, postB, postC])
+        session.commit()
+
+        response = self.client.get("/api/posts?title_like=whistles",
+            headers=[("Accept", "application/json")]
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, "application/json")
+
+        posts = json.loads(response.data.decode("ascii"))
+        self.assertEqual(len(posts), 2)
+
+        post = posts[0]
+        self.assertEqual(post["title"], "Post with whistles")
+        self.assertEqual(post["body"], "Still a test")
+
+        post = posts[1]
+        self.assertEqual(post["title"], "Post with bells and whistles")
+        self.assertEqual(post["body"], "Another test")
+
+    def test_get_posts_with_body(self):
+        """ Filtering posts by body """
+        postA = models.Post(title="Just a test", body="Post with bells")
+        postB = models.Post(title="Still a Test", body="Post with whistles")
+        postC = models.Post(title="Another Test",
+                            body="Post with bells and whistles")
+
+        session.add_all([postA, postB, postC])
+        session.commit()
+
+        response = self.client.get("/api/posts?body_like=whistles",
+            headers=[("Accept", "application/json")]
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, "application/json")
+
+        posts = json.loads(response.data.decode("ascii"))
+        self.assertEqual(len(posts), 2)
+        
+        post = posts[0]
+        self.assertEqual(post["title"], "Still a Test")
+        self.assertEqual(post["body"], "Post with whistles")
+
+        post = posts[1]
+        self.assertEqual(post["title"], "Another Test")
+        self.assertEqual(post["body"], "Post with bells and whistles")  
+
 if __name__ == "__main__":
     unittest.main()
